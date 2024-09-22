1d406ea3ec9647af9ffc712e3e7aa0cb1247e2a4
dproshchaeva <darya.proshchaeva@yandex.ru>
2024-09-22 21:47:46 +0500

    upd

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dom = {
     selectbox: document.getElementById('selectbox'),
+    selectboxList: document.querySelector('.selectbox__list'),
     rooms: document.getElementById('rooms')
 }
 dom.selectbox.querySelector('.selectbox__selected').onclick = (event) => {
@@ -10,18 +11,27 @@ document.body.onclick = (event) => {
     const { target } = event
     if (
       !target.matches('.selectbox')
-        && !target.parentElement.matches('.selectbox')
-        && !target.parentElement.parentElement.matches('.selectbox')
+      && !target.parentElement.matches('.selectbox')
+      && !target.parentElement.parentElement.matches('.selectbox')
     ){
         dom.selectbox.classList.remove('open');
     }
 } 
-dom.selectbox.querySelector('.selectbox__list').onclick = (event) =>{
+dom.selectboxList.onclick = (event) => {
     const {target} = event
-    if (target.matches('selectbox__item')){
-        const value = target.dataset.room
-        dom.selectbox.classList.remove('open')
-        console.log(value)
+    if (target.matches('.selectbox__item')){
+      const { room } = target.dataset
+      const selectedItem = dom.selectboxList.querySelector('.selected')
+      selectedItem.classList.remove('selected')
+      target.classList.add('selected')
+      dom.selectbox.classList.remove('open')
+      selectRoom(room)
+    }
+}
+function selectRoom(room) {
+  const selectedRoom = dom.rooms.querySelector('.selected');
+  if (selectedRoom) {
+    selectedRoom.classList.remove('selected');
     }
 }
 
