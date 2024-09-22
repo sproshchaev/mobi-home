b8c05f00871b420e0700d5120dd2a65b035e2d57
dproshchaeva <darya.proshchaeva@yandex.ru>
2024-09-22 20:47:29 +0500

    upd

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const dom = {
     rooms: document.getElementById('rooms')
 }
 dom.selectbox.querySelector('.selectbox__selected').onclick = (event) => {
-    if (!dom.selectbox.matches('.open')) {
-        dom.selectbox.classList.add('open')
+    dom.selectbox.classList.toggle('open')
+}
+
+document.body.onclick = (event) => {
+    const { target } = event
+    if (
+      !target.matches('.selectbox')
+        && !target.parentElement.matches('.selectbox')
+        && !target.parentElement.parentElement.matches('.selectbox')
+    ){
+        dom.selectbox.classList.remove('open');
+    }
+} 
+dom.selectbox.querySelector('.selectbox__list').onclick = (event) =>{
+    const {target} = event
+    if (target.matches('selectbox__item')){
+        const value = target.dataset.room
+        dom.selectbox.classList.remove('open')
+        console.log(value)
     }
 }
+
