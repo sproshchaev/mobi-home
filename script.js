d0fb6a81da1dc2abb53428217fa5ebc3628db626
dproshchaeva <darya.proshchaeva@yandex.ru>
2024-09-23 20:16:06 +0500

    upd

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const dom = {
 dom.selectbox.querySelector('.selectbox__selected').onclick = (event) => {
     dom.selectbox.classList.toggle('open')
 }
+const rooms = {
+    all: 'Все комнаты',
+    livingroom: 'Зал',
+    bedroom : 'Спальня',
+    kitchen: 'Кухня',
+    bathroom: 'Ванная',
+    studio: 'Кабинет',
+    washingroom: 'Уборная',
+}
 
 document.body.onclick = (event) => {
     const { target } = event
@@ -28,10 +37,46 @@ dom.selectboxList.onclick = (event) => {
       selectRoom(room)
     }
 }
+
+
+
 function selectRoom(room) {
   const selectedRoom = dom.rooms.querySelector('.selected');
   if (selectedRoom) {
     selectedRoom.classList.remove('selected');
+    selectedSelectboxRoom.classList.remove('selected')
+  }
+  if (room != 'all') {
+    const newSelectedRoom = dom.rooms.querySelector(`[data-room=${room}]`);
+    newSelectedRoom.classList.add('selected');
+    renderScreen(false)
+  } else {
+    renderScreen(true)
+
+  }
+    const selectedSelectboxRoom = dom.selectbox.querySelector('.selectbox__item.selected');
+    selectedSelectboxRoom.classList.remove('selected');
+    const newSelectedItem = dom.selectbox.querySelector(`[data-room=${room}]`);
+    newSelectedItem.classList.add('selected');
+    const selectboxSelected = dom.selectbox.querySelector('.selectbox__selected span')
+    selectboxSelected.innerText = rooms[room]
+
+}
+
+dom.rooms.querySelectorAll('.room').forEach(room => {
+    room.onclick = (event) => {
+    const value = room.dataset.room
+    selectRoom(value)
     }
+})
+
+function renderScreen(isRooms){
+    setTimeout(() => {
+    if (isRooms){
+        dom.rooms.style.display = 'grid'
+    } else {
+        dom.rooms.style.display = 'none'
+    }
+    }, 300)
 }
 
